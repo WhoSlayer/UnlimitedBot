308603fab4af66ce86ee68b20639857a4851c4a0
Muzammil <63346753+WhoSlayer@users.noreply.github.com>
2023-10-16 12:42:19 +0100

    Update poll.js

--- Documents/Unlimited/fun/poll.js
+++ Documents/Unlimited/fun/poll.js
@@ -10,12 +10,12 @@ module.exports.run = async(bot, message, args) => {
     message.delete().catch();
     if(!message.member.hasPermission("MANAGE_CHANNELS")) return;
            const say = args.slice(1).join(' '); 
-           const evsd = new Discord.MessageEmbed()
+           const newMessage = new Discord.MessageEmbed()
            .setFooter(`Poll initiated by ${message.author.tag}`)
            .setTitle(`${say}`)
            .setColor(`${colours.CYAN}`)
-    //    message.channel.send(evsd);
-        const m = await message.channel.send(evsd);
+    //    message.channel.send(newMessage);
+        const m = await message.channel.send(newMessage);
 
         const reacted = await promptMessage(m, message.author, 2, reactions);
         const reacted1 = await promptMessage(m, message.author, 2, reactions1);
