b8f1f25a079d3b80c04d3418251a2efaf0e052f9
Muzammil <63346753+WhoSlayer@users.noreply.github.com>
2023-10-16 12:54:29 +0100

    Update removerole.js

--- Documents/Unlimited/general/removerole.js
+++ Documents/Unlimited/general/removerole.js
@@ -13,8 +13,8 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_ROLES", "ADMINISTRATOR")){ return message.channel.send("You dont have permission to perform this command!")}
 
     let rMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-    const bro = new Discord.MessageEmbed().setDescription("<:unsuccess:711232495592538124> | **Please provide a user to remove a role to.**").setColor('RED')
-    if(!rMember) return message.channel.send(bro)
+    const selectRole = new Discord.MessageEmbed().setDescription("<:unsuccess:711232495592538124> | **Please provide a user to remove a role to.**").setColor('RED')
+    if(!rMember) return message.channel.send(selectRole)
     let role = message.guild.roles.cache.find(r => r.name == args.slice(1).join(" ")) || message.guild.roles.cache.find(r => r.id == args.slice(1).join(" ")) || message.mentions.roles.first()
     const what = new Discord.MessageEmbed().setDescription("<:unsuccess:711232495592538124> | **Please provide a role to remove to said user.**").setColor('RED')
     if(!role) return message.channel.send(what) 
@@ -22,14 +22,14 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_ROLES") || !message.guild.owner) return message.channel.send('Uh oh, it seems like you do not have permission to use this command.')
 
     if(rMember.roles.cache.has(role)) {
-        let eeoo = new Discord.MessageEmbed().setDescription(`<:unsuccess:711232495592538124> | **${rMember.user.tag}** doesn't have that role!`).setColor('RED')
-        return message.channel.send(eeoo)
+        let hasRole = new Discord.MessageEmbed().setDescription(`<:unsuccess:711232495592538124> | **${rMember.user.tag}** doesn't have that role!`).setColor('RED')
+        return message.channel.send(hasRole)
     } else {
        // message.delete();
 
         await rMember.roles.remove(role.id).catch(e => console.log(e.message))
-        let erer = new Discord.MessageEmbed().setDescription(`<:success:710968598386049044> **${rMember.user.tag}** | - ${role.name}`).setColor('GREEN')
-        message.channel.send(erer)
+        let green = new Discord.MessageEmbed().setDescription(`<:success:710968598386049044> **${rMember.user.tag}** | - ${role.name}`).setColor('GREEN')
+        message.channel.send(green)
     }
 
     let embed = new Discord.MessageEmbed()
