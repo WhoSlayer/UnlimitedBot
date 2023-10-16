e40e60864a02c61163ae489210c21d36e8540cc0
Muzammil <63346753+WhoSlayer@users.noreply.github.com>
2023-10-16 12:59:50 +0100

    Update botinfo.js

--- Documents/Unlimited/statistics/botinfo.js
+++ Documents/Unlimited/statistics/botinfo.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
 
 let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
 
-const slayer = new Discord.MessageEmbed()
+const developerinfo = new Discord.MessageEmbed()
 .setTitle("Bot Information:")
 .setColor("RANDOM")
 .setThumbnail("https://images-ext-1.discordapp.net/external/68Q_bMlp8vjjbQgh-MB1cUuhWn3Y1SKP0Il7v5wtK2c/https/cdn.discordapp.com/avatars/705086721737359502/dead45d157cb714f1bd200ca28ad7564.webp")
@@ -23,7 +23,7 @@ const slayer = new Discord.MessageEmbed()
 .addField("URLs:", "[**Support Server**](https://discord.gg/yDB2Ekv) \n[**Bot Invite**](https://discord.com/api/oauth2/authorize?client_id=705086721737359502&permissions=2147479030&scope=bot)")
 .addField("Bot Statistics:", `**Members:** ${bot.users.cache.size}\n**Guilds:** ${bot.guilds.cache.size}\n**Creation date:** April 29 2020\n**Online for:** ${uptime}`)
 
-message.channel.send(slayer)
+message.channel.send(developerinfo)
 }
 
 module.exports.config = {
