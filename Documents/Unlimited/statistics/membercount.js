d19b6cbf7543d852908b73f93ca1b418e86f377d
Muzammil <63346753+WhoSlayer@users.noreply.github.com>
2023-10-16 13:00:21 +0100

    Update membercount.js

--- Documents/Unlimited/statistics/membercount.js
+++ Documents/Unlimited/statistics/membercount.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    const merm = new Discord.MessageEmbed()
+    const count = new Discord.MessageEmbed()
         .setAuthor(message.guild.name)
         .addField("Members:", `${message.guild.memberCount}`)
         .addField("Humans:", `${message.guild.members.cache.filter(member => !member.user.bot).size}`)
         .addField("Bots:", `${message.guild.memberCount - message.guild.members.cache.filter(member => !member.user.bot).size}`)
         .setColor('RED')
-    message.channel.send(merm)
+    message.channel.send(count)
 }
 
 
